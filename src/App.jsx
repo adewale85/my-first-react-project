800f80fca3e52fa7e777d1928286148d51617153
Adewale-Iseoluwa <iseoluwaodu85@email.com>
2025-10-08 04:04:40 -0700

    create: a components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
@@ -6,6 +7,7 @@ const App = () => {
        <h1 className="text-3xl font-bold underline text-red-500 bg-blue-700">
       Hello world!
     </h1>
+    <Navbar/>
     </div>
   )
 }
